abe6294adc213d2b34b6d6fa809a44ca8ae10611
Twiggiermaen21 <kacper.pudelko.kpk@gmail.com>
2025-03-15 16:58:59 +0100

    hero 1.1

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './Hero.css'
-
+import dark_arrow from '../../assets/dark-arrow.png'
 
 const Hero = () => {
     return (
-        <div className='hero'>
+        <div className='hero container'>
 
+            <div className="hero-text">
+                <h1>We dasdasdasdasdsa</h1>
+                <p> sasdhausd ajisdjais daisdoj aisd aisdj asidj aisdiasdiasidjiad
+                    daidojaoidjaisdjioasdjiaidiadioasdisaid  ajisdjiad
+                    siadiaodiaiodjaioisadj </p>
+                <button className='btn'>Explore more <img src={dark_arrow} alt="" /></button>
 
+            </div>
 
         </div>
     )
