88697de2bbb575c0f715ab0d8dbae339b130447b
Twiggiermaen21 <kacper.pudelko.kpk@gmail.com>
2025-03-16 20:04:38 +0100

    footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p>© 2024 Edisity. All right reserved.</p>
+            <ul>
+                <li>Term of services</li>
+                <li>Privacy Policy</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Footer
